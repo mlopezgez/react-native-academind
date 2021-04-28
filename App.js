39b4c59b1fd207e85f93523c8f71598fb97a17f4
Matías López <milopez3@miuandes.cl>
2021-04-28 14:57:31 -0400

    first commit

--- App.js
+++ App.js
@@ -1,61 +1,12 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { StyleSheet, View, Button, FlatList } from "react-native";
-
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { View, Text } from "react-native";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
 
-  const addGoalHandler = (goalTitle) => {
-    setCourseGoals((currentGoals) => [
-      ...currentGoals,
-      { id: uuidv4(), value: goalTitle },
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeGoalHandler = (goalId) => {
-    setCourseGoals((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id !== goalId);
-    });
-  };
-
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
-
-  
   return (
-    <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        onAddGoal={addGoalHandler}
-        onCancel={cancelGoalAdditionHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={(itemData) => (
-          <GoalItem
-            id={itemData.item.id}
-            title={itemData.item.value}
-            onDelete={removeGoalHandler}
-          />
-        )}
-      />
+    <View>
+      <Text> Wena compare</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    paddingTop: 50,
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingBottom: 50,
-  },
-});
